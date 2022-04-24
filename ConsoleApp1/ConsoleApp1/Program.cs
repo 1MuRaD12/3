a962f1250d546036f0a6b6548be07c1a6c97b1c5
1MuRaD12 <tu71tfsws@code.edu.az>
2022-04-24 04:24:12 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Fullname");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("Enter Subject");
+            string subject = Console.ReadLine();
+            Console.WriteLine("Enter GroupName");
+            string groupname = Console.ReadLine();
+            Student student = new Student(fullname, subject);
+            Console.WriteLine(student.ToString());
+            student.GroupName(groupname);
+            Console.WriteLine("\n--------------------------------\n");
+            Console.WriteLine("Enter Fullname");
+            string fullname1 = Console.ReadLine();
+            Console.WriteLine("Enter Subject");
+            string subject1 = Console.ReadLine();
+            Console.WriteLine("Enter GroupName");
+            string groupname1 = Console.ReadLine();         
+            Student student1 = new Student(fullname1, subject1);
+            Console.WriteLine(student1.ToString());
+            student1.GroupName(groupname1);
+          
+            
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public static int Count;
+
+        public string Fullname { get; set; }
+
+        public string Subject { get; set; }
+
+        public static int StudentId = 1000;
+
+        public Student(string fullname, string subject)
+        {
+            Count++;
+            Id = Count;
+            Fullname = fullname;
+            Subject = subject;
+        }
+        public void GroupName(string word)
+        {
+            StudentId++;
+            word = word.Remove(2).ToUpper() + StudentId;
+            Console.WriteLine(word);
+        }
+        public string Showinfo()
+        {
+            return $"Fullname:{Fullname}\nSubject:{Subject}\nId:{Id}";
+        }
+        public override string ToString()
+        {
+            return Showinfo();
+        }
+    }
+}
